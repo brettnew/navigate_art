28b75bff58b5e80cc08782085e25f71df9cbdf52
Brett New <jumpingneptune@gmail.com>
2020-02-17 16:31:33 -0600

    Allow user to change routes via popup menu button

--- lib/art_route.dart
+++ lib/art_route.dart
@@ -9,6 +9,21 @@ class ArtRoute extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text('Navigating art'),
+          actions: <Widget>[
+            PopupMenuButton(
+              // icon: Icon(Icons.image),
+              // child: Text('Change Route'),
+              itemBuilder: (BuildContext context) {
+                return ArtUtil.menuItems.map((String item) {
+                  return PopupMenuItem<String>(
+                    child: Text(item),
+                    value: item,
+                  );
+                }).toList();
+              },
+              onSelected: (value) => changeRoute(context, value),
+            )
+          ],
         ),
         body: Container(
           decoration: BoxDecoration(
@@ -18,5 +33,28 @@ class ArtRoute extends StatelessWidget {
             ),
           ),
         ));
+  }
+
+  void changeRoute(BuildContext context, String menuItem) {
+    String image;
+    switch (menuItem) {
+      case ArtUtil.CARAVAGGIO:
+        {
+          image = ArtUtil.IMG_CARAVAGGIO;
+          break;
+        }
+      case ArtUtil.MONET:
+        {
+          image = ArtUtil.IMG_MONET;
+          break;
+        }
+      case ArtUtil.VANGOGH:
+        {
+          image = ArtUtil.IMG_VANGOGH;
+          break;
+        }
+    }
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => ArtRoute(art: image)));
   }
 }
